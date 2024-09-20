29037027b1541e0d3847e75f9f75617806676a33
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:37:11 -0400

    Change organization to zopencommunity

--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/netpbmport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/netpbmport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/netpbmport.git'), string(name: 'PORT_DESCRIPTION', value: 'Netpbm is a toolkit for manipulation of graphic images, including conversion of images between a variety of different formats.' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/netpbmport.git'), string(name: 'PORT_DESCRIPTION', value: 'Netpbm is a toolkit for manipulation of graphic images, including conversion of images between a variety of different formats.' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
   }
 }
